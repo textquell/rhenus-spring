117863af3dcaa43098f678bfd731ecd148ff4089
Hans Meyer <hamster.of.dev@googlemail.com>
2013-02-09 17:43:49 +0100

    Add unit tests
    In order to test what is developed, fill the test step in the build
    process. Assume that we are using NUnit as provided by nant, have a
    "tests" folder under src to contain the same structure of classes as
    their regarding tests and write an initial test

---
+++ src/tests/Service.Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Rhenus
+{
+    namespace Spring
+    {
+        [TestFixture]
+        public class ServiceTest
+        {
+            [Test]
+            public void firstTest()
+            {
+                Assert.True( true );
+            }
+        }
+    }
+}
