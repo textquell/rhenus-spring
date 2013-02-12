23f0e2c4c80f9d0860657a702e4ef2d80eb7933d
Hans Meyer <hamster.of.dev@googlemail.com>
2013-02-12 20:00:27 +0100

    Add dynamic properties to a config file
    The service has to be configurable and all properties that are changing
    from deployment to deployment should be outsourced to a config file.
    Create a basic implementation of this config file and load it in the
    service main to check if it's working.

---
+++ src/config/Settings.Designer.cs
@@ -0,0 +1,32 @@
+﻿namespace Rhenus
+{
+    namespace Spring
+    {
+        namespace Configuration
+        {
+            internal sealed partial class Settings: global::System.Configuration.ApplicationSettingsBase
+            {
+
+                private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized( new Settings() )));
+
+                public static Settings Default
+                {
+                    get
+                    {
+                        return defaultInstance;
+                    }
+                }
+
+                [global::System.Configuration.ApplicationScopedSettingAttribute()]
+                [global::System.Configuration.DefaultSettingValueAttribute( "Some Setting" )]
+                public string MyFirstSetting
+                {
+                    get
+                    {
+                        return ((string)(this["MyFirstSetting"]));
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/config/Settings.cs
@@ -0,0 +1,40 @@
+﻿namespace Rhenus {
+
+    namespace Spring
+    {
+        namespace Configuration
+        {
+            internal sealed partial class Settings
+            {
+
+                public Settings()
+                {
+                    this.SettingChanging += this.SettingChangingEventHandler;
+                    this.PropertyChanged += this.PropertyChangedEventHandler;
+                    this.SettingsLoaded += this.SettingsLoadedEventHandler;
+                    this.SettingsSaving += this.SettingsSavingEventHandler;
+                }
+
+                private void SettingsLoadedEventHandler( object sender, System.Configuration.SettingsLoadedEventArgs e )
+                {
+                    System.Diagnostics.Trace.TraceInformation("Settings loaded");
+                }
+
+                private void PropertyChangedEventHandler( object sender, System.ComponentModel.PropertyChangedEventArgs e )
+                {
+                    System.Diagnostics.Trace.TraceInformation( "Property changed" );
+                }
+
+                private void SettingChangingEventHandler( object sender, System.Configuration.SettingChangingEventArgs e )
+                {
+                    System.Diagnostics.Trace.TraceInformation( "Settings changing" );
+                }
+
+                private void SettingsSavingEventHandler( object sender, System.ComponentModel.CancelEventArgs e )
+                {
+                    System.Diagnostics.Trace.TraceInformation( "Settings saved" );
+                }
+            }
+        }
+    }
+}
