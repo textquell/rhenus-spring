ba206a08cad23717b8523ab535b89c1aea992a09
Hans Meyer <hamster.of.dev@gmail.com>
2013-02-24 10:58:23 +0100

    Refactoring

--- src/SimpleTaskScheduler.cs
+++ src/SimpleTaskScheduler.cs
@@ -25,73 +25,76 @@ using System.Collections.Generic;
 
 namespace Rhenus.Spring
 {
-    /// <summary>
-    /// A simple implementation of the <see cref="Rhenus.Spring.ITaskScheduler">TaskScheduler
-    /// </see> Interface.
-    /// </summary>
-    /// <remarks>
-    /// This class is working with the <see cref="System.Threading.ThreadPool">ThreadPool</see>
-    /// to execute tasks. When a task is scheduled without further details, its 
-    /// <see cref="Rhenus.Spring.ITask.Run">Run</see> method is handed over to the ThreadPool
-    /// for execution. 
-    /// </remarks>
-    class SimpleTaskScheduler: ITaskScheduler
-    {
-        public void ScheduleTask( ITask task )
-        {
-            // TODO: write a message when throwing this error. Get this message from a ressource file
-            if ( task == null ) { throw new System.ArgumentNullException(); }
-            ThreadPool.QueueUserWorkItem( new WaitCallback( task.Run ) );
-        }
+	/// <summary>
+	/// A simple implementation of the <see cref="Rhenus.Spring.ITaskScheduler">TaskScheduler
+	/// </see> Interface.
+	/// </summary>
+	/// <remarks>
+	/// This class is working with the <see cref="System.Threading.ThreadPool">ThreadPool</see>
+	/// to execute tasks. When a task is scheduled without further details, its 
+	/// <see cref="Rhenus.Spring.ITask.Run">Run</see> method is handed over to the ThreadPool
+	/// for execution. 
+	/// </remarks>
+	class SimpleTaskScheduler: ITaskScheduler
+	{
+		public void ScheduleTask (ITask task)
+		{
+			// TODO: write a message when throwing this error. Get this message from a ressource file
+			if (task == null) {
+				throw new System.ArgumentNullException ();
+			}
+			ThreadPool.QueueUserWorkItem (new WaitCallback (task.Run));
+		}
 
-        public void ScheduleTask( ITask task, DateTime startTime )
-        {
-            if ( task == null ) { throw new System.ArgumentNullException("Task parameter cannot be null"); }
-            if ( startTime <= DateTime.Now ) { throw new System.ArgumentException("Starttime already passed or is DateTime.Now"); }
-            try
-            {
-                DelayedTask taskToExecute = new DelayedTask( task, startTime );
-            }
-            catch ( ArgumentOutOfRangeException exception )
-            {
-                throw new ArgumentNullException( "Delay was to short", exception );
-            }
-        }
+		public void ScheduleTask (ITask task, DateTime startTime)
+		{
+			if (task == null) {
+				throw new System.ArgumentNullException ("Task parameter cannot be null");
+			}
+			if (startTime <= DateTime.Now) {
+				throw new System.ArgumentException ("Starttime already passed or is DateTime.Now");
+			}
+			try {
+				DelayedTask taskToExecute = new DelayedTask (task, startTime);
+			} catch (ArgumentOutOfRangeException exception) {
+				throw new ArgumentNullException ("Delay was to short", exception);
+			}
+		}
 
-        /// <summary>
-        /// Delays a task with a <see cref="System.Threading.Timer"/>.
-        /// </summary>
-        /// <remarks>
-        /// A delayed task takes the scheduled task and the start time are constructor arguments.
-        /// When it is created, it starts a new timer and waits till it elapses before queueing the 
-        /// task for execution by the ThreadPool
-        /// </remarks>
-        private class DelayedTask
-        {
-            private ITask taskToRun;
-            private Timer timer;
+		/// <summary>
+		/// Delays a task with a <see cref="System.Threading.Timer"/>.
+		/// </summary>
+		/// <remarks>
+		/// A delayed task takes the scheduled task and the start time are constructor arguments.
+		/// When it is created, it starts a new timer and waits till it elapses before queueing the 
+		/// task for execution by the ThreadPool
+		/// </remarks>
+		private class DelayedTask
+		{
+			private ITask taskToRun;
+			private Timer timer;
 
-            /// <summary>
-            /// Convenience constructor
-            /// </summary>
-            /// <param name="task">the scheduled task</param>
-            /// <param name="startTime">the time at which the task should run.</param>
-            public DelayedTask( ITask task, DateTime startTime )
-            {
-                this.taskToRun = task;
-                // period of -1 indicates to call the method only once.
-                timer = new Timer( new TimerCallback( taskToRun.Run ), null, startTime - DateTime.Now, new TimeSpan( -1 ) );
-            }
+			/// <summary>
+			/// Convenience constructor
+			/// </summary>
+			/// <param name="task">the scheduled task</param>
+			/// <param name="startTime">the time at which the task should run.</param>
+			public DelayedTask (ITask task, DateTime startTime)
+			{
+				this.taskToRun = task;
+				// period of -1 indicates to call the method only once.
+				timer = new Timer (new TimerCallback (taskToRun.Run), null, startTime - DateTime.Now, new TimeSpan (-1));
+			}
 
-            /// <summary>
-            /// Enqueues the task to the ThreadPool.
-            /// </summary>
-            private void timeToRun( object sender, System.Timers.ElapsedEventArgs args )
-            {
-                ThreadPool.QueueUserWorkItem( new WaitCallback( taskToRun.Run ) );
-                this.timer.Dispose();
-            }
+			/// <summary>
+			/// Enqueues the task to the ThreadPool.
+			/// </summary>
+			private void timeToRun (object sender, System.Timers.ElapsedEventArgs args)
+			{
+				ThreadPool.QueueUserWorkItem (new WaitCallback (taskToRun.Run));
+				this.timer.Dispose ();
+			}
 
-        }
-    }
+		}
+	}
 }
